a997009e46d6f3f338d0f28fcde0b10cec0c3438
Tarcisio Coutinho <tcs5cin@gmail.com>
2023-04-10 12:03:03 -0300

    add jmx endpoints

--- exercise-01/api/src/main/kotlin/io/tacsio/adaptive/ApiApplication.kt
+++ exercise-01/api/src/main/kotlin/io/tacsio/adaptive/ApiApplication.kt
@@ -3,6 +3,7 @@ package io.tacsio.adaptive
 import io.tacsio.adaptive.mapek.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -12,17 +13,19 @@ import org.springframework.context.annotation.Bean
 class ApiApplication {
 
     @Bean
-    fun startManagement() = CommandLineRunner {
+    fun startManagement(@Value("\${mapek.enabled:false}") enabled: Boolean) = CommandLineRunner {
         val knowledge = Knowledge()
         val executor = Executor(knowledge)
         val planner = Planner(knowledge, executor.executorInChannel)
         val analyzer = Analyzer(knowledge, planner.plannerInChannel)
         val monitor = Monitor(knowledge, analyzer.analyzerInChannel)
 
-        GlobalScope.launch { monitor.start() }
-        GlobalScope.launch { analyzer.start() }
-        GlobalScope.launch { planner.start() }
-        GlobalScope.launch { executor.start() }
+        if (enabled) {
+            GlobalScope.launch { monitor.start() }
+            GlobalScope.launch { analyzer.start() }
+            GlobalScope.launch { planner.start() }
+            GlobalScope.launch { executor.start() }
+        }
     }
 }
 
